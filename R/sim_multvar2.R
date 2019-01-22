8ebaa45a7a836d6e459812a1bf54e984b8d6e23f
Aariq <scottericr@gmail.com>
2019-01-22 12:52:25 -0600

    minor change to check this is working

--- R/sim_multvar2.R
+++ R/sim_multvar2.R
@@ -1,14 +1,14 @@
-#' Simulate a dataset with a given number of observations and groups
+#' Simulate a dataset with a given number of observations and groups.
 #'
 #' @description This is a simple wrapper that creates a tibble of length `N` with a single column `groups`.  It will warn if there are fewer than three replicates per group.
 #' @param N Total number of observations/rows
 #' @param n_groups How many groups or treatments to simulate
 #'
 #' @return a tibble
-#' 
+#'
 #' @import tibble
 #' @import dplyr
-#' 
+#'
 #' @export
 #'
 #' @examples
@@ -44,11 +44,11 @@ set_diag <- function(x, value){
 #' @param seed an optional seed for random number generation.  If `NA` a random seed will be used.
 #'
 #' @return a tibble
-#' 
+#'
 #' @importFrom MASS mvrnorm
 #' @import dplyr
 #' @importFrom stats runif
-#' 
+#'
 #' @export
 #'
 #' @examples
@@ -65,11 +65,11 @@ sim_covar <- function(df, p, var, cov, name = NA, seed = NA) {
   }
   N <- nrow(df)
   S <- matrix(rep(cov, p^2), ncol = p) %>% set_diag(var)
-  
+
   set.seed(seed)
   sim_data <- MASS::mvrnorm(n = N, Sigma = S, mu = rep(0, p)) %>%
     as.data.frame()
-  
+
   if(!is.na(name)){
     colnames(sim_data) <- paste0(name, "_", 1:p)
   }
@@ -91,14 +91,14 @@ sim_covar <- function(df, p, var, cov, name = NA, seed = NA) {
 #' @param group specify the name of the column containing grouping variables
 #' @param name an optional name to be appended to the column names in the output
 #' @param seed an optional seed for random number generation.  If `NA` a random seed will be used.
-#' 
+#'
 #' @return a tibble
-#' 
+#'
 #' @importFrom MASS mvrnorm
 #' @importFrom stats runif
 #' @import dplyr
 #' @import purrr
-#' 
+#'
 #' @export
 #'
 #' @examples
@@ -113,13 +113,13 @@ sim_discr <- function(df, p, var, cov, group_means, group = "group", name = NA, 
   n_groups <- select(df, !!group_var) %>% unique() %>% nrow()
   #arrange by grouping variable for easier merging later
   df <- arrange(df, !!group_var)
-  
+
   #number of observations in each group
-  len_groups <- df %>% 
-    group_by(!!group_var) %>% 
-    summarise(lens = n()) %>% 
+  len_groups <- df %>%
+    group_by(!!group_var) %>%
+    summarise(lens = n()) %>%
     .$lens
-  
+
   if(length(group_means) != n_groups){
     stop(paste("Please supply means for all", n_groups, "groups."))
   }
@@ -135,23 +135,23 @@ sim_discr <- function(df, p, var, cov, group_means, group = "group", name = NA, 
   if(length(cov) == 1){
     Cov <- rep(cov, n_groups)
   }
-  
+
   #list of vcov matrices for each group
   S.list <- purrr::map2(Cov, Var,
                         ~matrix(rep(.x, p^2), ncol = p) %>%
                           set_diag(.y))
   #list of all params for pmap_df()
   l <- list(S = S.list, n = len_groups, mu = group_means)
-  
+
   set.seed(seed)
   #for each group, use that groups vcov matrix, sample size, and group mean in mvrnorm
   sim_data <- pmap_df(l, .f = function(S, n, mu){
     MASS::mvrnorm(n = n,
                   Sigma = S,
-                  mu = rep(mu, p)) %>% 
+                  mu = rep(mu, p)) %>%
       as.data.frame
   })
-  
+
   if(!is.na(name)){
     colnames(sim_data) <- paste0(name, "_", 1:p)
   }
@@ -166,36 +166,36 @@ sim_discr <- function(df, p, var, cov, group_means, group = "group", name = NA, 
 #' @param prop proportion of values to be set to NA
 #'
 #' @return a dataframe with NAs
-#' 
+#'
 #' @import dplyr
 #' @importFrom stats runif
-#' 
+#'
 #' @export
 #'
 #' @examples
 #' library(dplyr)
-#' df <- sim_df(10, 2) %>% 
-#' sim_covar(10, 1, 0.5) %>% 
+#' df <- sim_df(10, 2) %>%
+#' sim_covar(10, 1, 0.5) %>%
 #' sim_missing(0.05)
-sim_missing <- 
+sim_missing <-
   function(df, prop, seed = NA){ #consider adding vars= to specify what columns.  Pass to select()
     if(is.na(seed)){
       seed = as.integer(runif(1) * 10e6)
     }
     datadim <- df %>%
-      select_if(is.double) %>% 
+      select_if(is.double) %>%
       dim()
-    
+
     size = datadim[1] * datadim[2]
-  
+
     na.num = round(size * prop)
     if(na.num == 0){
       warning("No NA's will be introduced.  Select a larger proportion to introduce NAs")
     }
-    
+
     set.seed(seed)
     na.vector <- sample(c(rep(NA, na.num), rep(0, size - na.num)), size)
-    
+
     mask <- matrix(na.vector, nrow = datadim[1], ncol = datadim[2])
     newdata <- df %>% select_if(is.double) + mask
     newdf <- bind_cols(select_if(df, ~!is.double(.)), newdata)
