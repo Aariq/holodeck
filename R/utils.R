89c8f1406faa34763b0437407f78764412f0e2ce
Aariq <scottericr@gmail.com>
2019-01-28 11:26:34 -0500

    moved set_diag() into utils.R and wrote propper documentation.

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,22 @@
 #' @param x An object to test.
 #' @param lhs,rhs Expressions for the LHS and RHS of the definition.
 NULL
+
+
+# pipe-able wrapper to diag()
+#' Pipe friendly wrapper to `diag(x) <- value`
+#'
+#' @param x a matrix
+#' @param value either a single value or a vector of length equal to the diagonal of `x`.
+#'
+#' @return a matrix
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' matrix(0,3,3) %>%
+#' set_diag(1)
+set_diag <- function(x, value){
+  diag(x) <- value
+  return(x)
+}
