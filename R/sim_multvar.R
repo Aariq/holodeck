8a7b563327bf6ba1889bb1ddc4bfd8545859ec71
Aariq <scottericr@gmail.com>
2019-01-31 11:38:17 -0500

    finished this branch. I think I like it.

--- R/sim_multvar.R
+++ R/sim_multvar.R
@@ -1,11 +1,13 @@
-#' Simulate a dataset with a given number of observations and groups
+#' Simulate categorical data
 #'
-#' To-do: make this optionally accpet a dataframe and add categorical variables
-#' Make this optionally create multiple categorical variables as being nested or crossed or random
+#' To-do:
+#'
+#' - Make this optionally create multiple categorical variables as being nested or crossed or random
 #'
 #' @description This is a simple wrapper that creates a tibble of length `N` with a single column `groups`.  It will warn if there are fewer than three replicates per group.
-#' @param N Total number of observations/rows
-#' @param n_groups How many groups or treatments to simulate
+#' @param .data An optional dataframe.  If a dataframe is supplied, simulated categorical data will be added to the dataframe. Either `.data` or `N` must be supplied.
+#' @param N Total number of observations/rows to simulate if `.data` is not supplied.
+#' @param n_groups How many groups or treatments to simulate.
 #' @param name The column name for the grouping variable.  Defaults to "group".
 #'
 #' @return a tibble
@@ -16,9 +18,22 @@
 #' @export
 #'
 #' @examples
-#' df <- sim_cat(30, 3)
-sim_cat <- function(N, n_groups, name = "group") {
+#' df <- sim_cat(N = 30, n_groups = 3)
+sim_cat <- function(.data = NULL, N = NULL, n_groups, name = "group") {
   name <- sym(name)
+  #assert that either .data or N is supplied, not neither nor both
+  if(is.null(.data) & is.null(N)) {
+    stop("You must supply either a dataframe to '.data' or 'N'")
+  }
+  if(!is.null(.data) & !is.null(N)) {
+    stop("Please supply either '.data' or 'N', not both")
+  }
+  # if data is supplied, check that it's a dataframe and set N
+  if(is.null(N)) {
+    assertthat::assert_that(is.data.frame(.data))
+    N <- nrow(.data)
+  }
+  ## Consider moving this check to sim_discr()
   #stopifnots for group size.  Groups should have at least 3 obserations, warn if less than 5.
   if(N/n_groups < 3){
     stop("Not enough replicates per group")
@@ -29,21 +44,23 @@ sim_cat <- function(N, n_groups, name = "group") {
   df <-
     tibble(!!name := rep(letters[1:n_groups], length.out = N)) %>%
     arrange(!!name)
-
+  if(!is.null(.data)){
+    df <- bind_cols(.data, df)
+  }
   return(df)
 }
-
 
 
 #' Simulate co-varying variables
 #'
 #' Adds a group of variables (columns) with a given variance and covariance to a data frame or tibble
 #'
-#' @param .data a data frame or tibble
-#' @param p number of variables to simulate
+#' @param .data An optional dataframe.  If a dataframe is supplied, simulated categorical data will be added to the dataframe. Either `.data` or `N` must be supplied.
+#' @param N Total number of observations/rows to simulate if `.data` is not supplied.
+#' @param p number of variables to simulate.
 #' @param var variance used to construct variance-covarinace matrix.
 #' @param cov covariance used to construct variance-covarinace matrix.
-#' @param name an optional name to be appended to the column names in the output
+#' @param name an optional name to be appended to the column names in the output.
 #' @param seed an optional seed for random number generation.  If `NA` (default) a random seed will be used.
 #'
 #' @return a tibble
@@ -56,28 +73,49 @@ sim_cat <- function(N, n_groups, name = "group") {
 #'
 #' @examples
 #' library(dplyr)
-#' sim_cat(30, 3) %>%
+#' sim_cat(N = 30, n_groups = 3) %>%
 #' sim_covar(p = 5, var = 1, cov = 0.5, name = "correlated")
 
-sim_covar <- function(.data, p, var, cov, name = NA, seed = NA) {
-  stopifnot(is.data.frame(.data))
+sim_covar <- function(.data = NULL, N = NULL, p, var, cov, name = NA, seed = NA) {
+  if(is.null(.data) & is.null(N)) {
+    stop("You must supply either a dataframe to '.data' or 'N'")
+  }
+  if(!is.null(.data) & !is.null(N)) {
+    stop("Please supply either '.data' or 'N', not both")
+  }
+  # if data is supplied, check that it's a dataframe and set N
+  if(is.null(N)) {
+    assertthat::assert_that(is.data.frame(.data))
+    N <- nrow(.data)
+  }
   if(length(cov) != 1){
     stop("Error: length(cov) not equal to 1")
   }
   if(is.na(seed)){
     seed = as.integer(runif(1) * 10e6)
   }
-  N <- nrow(.data)
+
   S <- matrix(rep(cov, p^2), ncol = p) %>% set_diag(var)
 
   set.seed(seed)
-  sim_data <- MASS::mvrnorm(n = N, Sigma = S, mu = rep(0, p)) %>%
-    as.data.frame()
+  sim_matrix <- MASS::mvrnorm(n = N, Sigma = S, mu = rep(0, p))
 
+  #set column names
   if(!is.na(name)){
-    colnames(sim_data) <- paste0(name, "_", 1:p)
+    colnames(sim_matrix) <- paste0(name, "_", 1:p)
+  } else {
+    colnames(sim_matrix) <- paste0("V", 1:p)
   }
-  new_df <- bind_cols(.data, sim_data)
+
+  sim_data <- sim_matrix %>%
+    as_tibble(.name_repair = "check_unique")
+
+  #either make a new df or bind to existing one
+  if(!is.null(.data)) {
+    new_df <- bind_cols(.data, sim_data)
+  } else {
+    new_df <- sim_data
+  }
   return(new_df)
 }
 
@@ -107,7 +145,7 @@ sim_covar <- function(.data, p, var, cov, name = NA, seed = NA) {
 #'
 #' @examples
 #' library(dplyr)
-#' sim_cat(30, 3) %>%
+#' sim_cat(N = 30, n_groups = 3) %>%
 #' sim_discr(p = 5, var = 1, cov = 0.5, group_means = c(-1, 0, 1), name = "descr")
 sim_discr <- function(.data, p, var, cov, group_means, group = "group", name = NA, seed = NA){
   if(is.na(seed)){
@@ -184,8 +222,8 @@ sim_discr <- function(.data, p, var, cov, group_means, group = "group", name = N
 #'
 #' @examples
 #' library(dplyr)
-#' df <- sim_cat(10, 2) %>%
-#' sim_covar(10, 1, 0.5) %>%
+#' df <- sim_cat(N = 10, n_groups = 2) %>%
+#' sim_covar(p = 10, var = 1, cov = 0.5) %>%
 #' sim_missing(0.05)
 sim_missing <-
   function(.data, prop, seed = NA){ #consider adding vars= to specify what columns.  Pass to select()
