60d835798a365c9bb93959490e41a48ff793e382
Eric R. Scott <scottericr@gmail.com>
2023-08-25 16:33:49 -0400

    update package-level documentation

--- R/package.R
+++
@@ -1,9 +0,0 @@
-#' holodeck: A package for simulating multivariate datasets
-#'
-#' The `holodeck` package contains functions for creating "chunks" of variables with different degrees of co-variance (collinearity) and discrimination among groups (i.e. levels of a categorical variable).
-#'
-#' What make it 'tidy'?  All `sim_*` functions accept dataframes or tibbles as their first argument and return tibbles, meaning they work with the pipe operator (`%>%`) from the `dplyr` package.
-#'
-#' @docType package
-#' @name holodeck
-NULL
